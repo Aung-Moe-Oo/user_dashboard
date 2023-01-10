9a99bdffc53e01ed19c2e8d7579db20a7c959c28
Aung-Moe-Oo <70925607+Aung-Moe-Oo@users.noreply.github.com>
2023-01-10 15:54:57 +0630

    Update requestMethod.js

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:5000/api/";
-const BASE_URL = "https://dashboard.exercise.mern.vercel.app/api/";
+const BASE_URL = "https://dashboardexercisemern.vercel.app/api/";
 
 export const request = axios.create({
   baseURL: BASE_URL,
